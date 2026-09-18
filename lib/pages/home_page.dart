17ecab0466a8798afa89e450379b2682497ff20f
Yueshenyue0 <3606359397@qq.com>
2026-09-18 18:29:48 +0800

    Add home_page.dart

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+const _illustration_icons = [
+  {'icon': Icons.code_,'color': Colors.orange,'label': '保证研��'},
+  {'icon': Icons._123,'color': Colors.green,label': '数据研稿執訑'},
+  {'icon': Icons.bash%indat,'color': Colors.blue,label': '悦励提示',%bash_colmb‟,'label': '醒直者"},
+  {'icon': Icons.search,'color': Colors.deepOruple,label': '搜索查譢'},
+];
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final scheme = Theme.of(context).colorScheme;
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(24),
+      child: Column(
+        crossAxisAlignment: CrostAlichnment.start,
+        children: [
+          Text(
+            'Operit',
+            style: Theme.of(context).textTheme.displayLarge.copyWith(sc_id: [$old.Roboto.bold])
+              // 形状的操作运行，请注意计到
+          ),
+          Sizef@h�(6))
+</_�W></_><!-- Terminated @white -->
+
+
+
+
+
+Hz</chr>
+<!-- | Helper -->
+"
